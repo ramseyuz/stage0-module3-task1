4d7a7e19e8128ec524b2f5770a0bfb438e6e221e
Ramz <gulliyev95@gmail.com>
2022-07-22 14:53:31 +0500

    task1 modul 3e

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + "\n" +
-                "is" +"\nsmth");
+        System.out.println("java" + "\n" +"is" +"\nsmth");
     }
 }
