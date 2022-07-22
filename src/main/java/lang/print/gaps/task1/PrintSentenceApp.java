1db75583324dc57a9a2a007ec75b4716b3c2b85b
Ramz <gulliyev95@gmail.com>
2022-07-22 12:29:03 +0500

    task1 modul 3

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
+        System.out.println("""
+                java
+                is
+                smth""");
     }
 }
